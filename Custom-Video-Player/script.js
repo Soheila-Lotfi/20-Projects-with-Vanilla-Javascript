b1da3c025c771f855745e40b4359b9436958b1dd
soheila <s.lotfiastanjin@gmail.com>
2020-05-21 17:04:31 -0400

    set video progress

--- Custom-Video-Player/script.js
+++ Custom-Video-Player/script.js
@@ -3,6 +3,7 @@
 const video = document.getElementById('video')
 const playBtn = document.getElementById('play')
 const stopBtn = document.getElementById('stop')
+const progress = document.getElementById('progress')
 
 
 // toggle video status
@@ -12,10 +13,12 @@ function toggleVideoStatus() {
     console.log(video.paused)
     if (video.paused) {
         video.play();
+        //update puase icon
         playBtn.innerHTML = '<i class="fa fa-play fa-2x">'
     }
     else {
         video.pause();
+        // update play icon
         playBtn.innerHTML = '<i class="fa fa-pause fa-2x">'
 
     }
@@ -29,10 +32,21 @@ function stopVideo() {
     video.pause();
 }
 
+// set video progress
+function setVideoProgress() {
+    // when progress.value is 100 which is max, video is over which is video.duration
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
 
 
 // Event listener
 
 video.addEventListener('click', toggleVideoStatus);
 playBtn.addEventListener('click', toggleVideoStatus);
-stopBtn.addEventListener('click', stopVideo)
+stopBtn.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
+
+
+
+
