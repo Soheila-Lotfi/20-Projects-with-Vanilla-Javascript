92a8987bf16385c25f8bdc313530e9bce7a3322d
soheila <s.lotfiastanjin@gmail.com>
2020-05-25 19:06:01 -0400

    display users on the page

--- Dom-Array-Methods/script.js
+++ Dom-Array-Methods/script.js
@@ -1,4 +1,6 @@
-
+// get the elements
+const addUser = document.getElementById('add-user')
+const main = document.getElementById('main')
 
 // get the random users
 
@@ -8,6 +10,7 @@ getRandomUsers();
 
 let users = [];
 
+// get the random users from an api usinf fetch
 async function getRandomUsers() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
@@ -17,5 +20,33 @@ async function getRandomUsers() {
         name: `${user.name.first} ${user.name.last}`,
         money: `$${Math.floor(Math.random() * 10000000000)}`
     }
-    users.push(newUser)
+    addData(newUser)
 }
+
+
+// add new user to data
+function addData(user) {
+    users.push(user);
+
+    // grab the new user and then update the dom, display it on the dom
+    updateDom();
+
+}
+
+// update dom
+
+function updateDom(providedUSer = users) {
+
+    // empty the main
+    main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`
+
+    providedUSer.forEach(item => {
+        const element = document.createElement('div')
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+        main.appendChild(element);
+    })
+
+
+}
+
