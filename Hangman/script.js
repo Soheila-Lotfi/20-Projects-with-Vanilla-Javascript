a9f16e9d88c23a99ed694ab672760ed9cde8d536
soheila <s.lotfiastanjin@gmail.com>
2020-06-02 15:56:33 -0400

    finish html and css- add event listener

---
+++ Hangman/script.js
@@ -0,0 +1,15 @@
+
+
+const words = ['apple', 'udemy', 'programming', 'interface']
+const selectedWord = words[Math.floor(Math.random() * words.length)]
+
+
+window.addEventListener('keydown', e => {
+
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+
+        const letter = e.key.toLowerCase();
+    }
+
+
+})
